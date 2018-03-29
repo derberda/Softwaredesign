1dc70a4ef4235ab071e3bb022630c85119d15bd4
derberda <derberdan@gmail.com>
2018-03-29 17:28:54 +0200

    Initial commit

---
+++ AFG1.1/AFG1.1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace AFG1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double v;
+
+
+            var Num = (args[1]);
+            double d = Convert.ToDouble(Num);
+
+            switch (args[0])
+            {
+                case "w":
+                    Console.WriteLine("Es werden die Daten für den Würfel berechnet!");
+                    a = getCubeSurface(d);
+                    v = getCubeVolume(d);
+                    getCubeInfo(a, v);
+                    break;
+                case "k":
+                    Console.WriteLine("Daten für die Kugel wierden berschnet");
+                    a = getSphereSurface(d);
+                    v = getSphereVolume(d);
+                    getSphereInfo(a, v);
+                    break;
+                case "o":
+                    Console.WriteLine("Es werden die Daten für den Octaeder berechnet");
+                    a = getOcSurface(d);
+                    v = getOcVolume(d);
+                    getOcInfo(a, v);
+                    break;
+                default:
+                    Console.WriteLine("Verstehe ich nicht");
+                    break;
+            }
+
+        }
+        //Methoden Würfel
+        public static double getCubeSurface(double durchmesser)
+        {
+            double d = durchmesser;
+            double a = Math.Round(6 * (d * d),2);
+            return a;
+        }
+        public static double getCubeVolume(double durchmesser)
+        {
+            double d = durchmesser;
+            double v = Math.Round((d * d * d),2);
+            return v;
+        }
+        public static void getCubeInfo(double surface, double volume)
+        {
+            double a = surface;
+            double v = volume;
+            Console.WriteLine("A=" + a + " | " + "V=" + v);
+        }
+
+        //Methoden Kugel
+        public static double getSphereSurface(double durchmesser)
+        {
+            double d = durchmesser;
+            double a = Math.Round(Math.PI * (d * d),2);
+            return a;
+        }
+        public static double getSphereVolume(double durchmesser)
+        {
+            double d = durchmesser;
+            double v = Math.Round((Math.PI * (d * d * d)) / 6,2);
+            return v;
+        }
+        public static void getSphereInfo(double surface, double volume)
+        {
+            double a = surface;
+            double v = volume;
+            Console.WriteLine("A=" + a + " | " + "V=" + v);
+        }
+
+        //Method Oktaeder
+        public static double getOcSurface(double durchmesser)
+        {
+            double d = durchmesser;
+            double a = Math.Round(2 * Math.Sqrt(3) * (d * d),2);
+            return a;
+        }
+        public static double getOcVolume(double durchmesser)
+        {
+            double d = durchmesser;
+            double v = Math.Round((Math.Sqrt(2) * (d * d * d)) / 3,2);
+            return v;
+        }
+        public static void getOcInfo(double surface, double volume)
+        {
+            double a = surface;
+            double v = volume;
+            Console.WriteLine("A=" + a + " | " + "V=" + v);
+        }
+    }
+}
---
+++ AFG1.2/AFG1.2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AFG1._2
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+        static string s;
+        static string v;
+        static string o;
+        static void Main(string[] args)
+        {
+            string[] str = new string[subjects.Length];
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                GetVerse();
+                str[i] = s + " " + v + " " + o;
+            }
+            for (int j = 0; j < subjects.Length; j++)
+            {
+                Console.WriteLine(str[j]);
+            }
+        }
+        public static void GetVerse()
+        {
+            // Console.WriteLine(" ");
+            // for (int i = 0; i < subjects.Length; i++)
+            // {
+            Random random = new Random();
+            int a = random.Next(0, subjects.Length);
+            int b = random.Next(0, verbs.Length);
+            int c = random.Next(0, objects.Length);
+            //Console.WriteLine(subjects[a] + " " + verbs[b] + " " + objects[c]);
+            // }
+            while (subjects[a] == "benutzt")
+            {
+                a = random.Next(0, subjects.Length);
+            }
+            while (verbs[b] == "benutzt")
+            {
+                b = random.Next(0, verbs.Length);
+            }
+            while (objects[c] == "benutzt")
+            {
+                c = random.Next(0, objects.Length);
+            }
+            s = subjects[a];
+            v = verbs[b];
+            o = objects[c];
+
+            subjects[a] = "benutzt";
+            verbs[b] = "benutzt";
+            objects[c] = "benutzt";
+        }
+    }
+}
