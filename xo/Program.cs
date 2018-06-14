f5100a27ebefda347f1505d399fc321022ce2717
derberda <derberdan@gmail.com>
2018-06-14 11:10:47 +0200

    xo formatiert

--- xo/Program.cs
+++ xo/Program.cs
@@ -4,9 +4,9 @@ namespace xo
 {
     class Program
     {
-        public static char[] gameData = {'1','2','3','4','5','6','7','8','9'};
+        public static char[] gameData = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         public static int counter = 0;
-        public static char[]turn = {'X','O'};
+        public static char[] turn = { 'X', 'O' };
         public static char player;
         public static int intInput;
 
@@ -20,9 +20,9 @@ namespace xo
         }
         public static void Game()
         {
-            for(;;)
-            {    
-                if(counter%2 == 0)
+            for (; ; )
+            {
+                if (counter % 2 == 0)
                 {
                     player = turn[0];
                 }
@@ -31,45 +31,45 @@ namespace xo
                     player = turn[1];
                 }
 
-                 PrintField();
-                 Console.WriteLine("It's your turn player " + player + " . Where do you want to set it?");
-                 string input = Console.ReadLine();
+                PrintField();
+                Console.WriteLine("It's your turn player " + player + " . Where do you want to set it?");
+                string input = Console.ReadLine();
 
-                 try
-                 {
-                     intInput = Convert.ToInt32(input);
-                     
-                     if(gameData[intInput-1] == turn[0] || gameData[intInput-1] == turn[1])
-                     {
+                try
+                {
+                    intInput = Convert.ToInt32(input);
+
+                    if (gameData[intInput - 1] == turn[0] || gameData[intInput - 1] == turn[1])
+                    {
                         Console.WriteLine("You must write another number between 1 and 9! This one is already occupied:(");
-                     }
-                     else
-                     {
-                         counter++;
-                         gameData[intInput-1] = player;
-                     }
-                 }
-                 catch (System.Exception)
-                 {
-                     Console.WriteLine("You must write a number between 1 and 9!");
-                 }
-                  if (FullField())
-                 {
-                     Console.WriteLine("It's a DRAW!");
-                     break;
-                 }
+                    }
+                    else
+                    {
+                        counter++;
+                        gameData[intInput - 1] = player;
+                    }
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("You must write a number between 1 and 9!");
+                }
+                if (FullField())
+                {
+                    Console.WriteLine("It's a DRAW!");
+                    break;
+                }
 
-                  if (Win())
-                 {
-                     Console.WriteLine("Player " + player + " won!");
-                     break;
-                 }
+                if (Win())
+                {
+                    Console.WriteLine("Player " + player + " won!");
+                    break;
+                }
             }
         }
 
         public static bool FullField()
         {
-            if(counter >= 9)
+            if (counter >= 9)
             {
                 return true;
             }
@@ -93,9 +93,9 @@ namespace xo
 
         public static void PrintField()
         {
-            Console.WriteLine("|" + gameData[0] + "|" + gameData[1] + "|" +  gameData[2] + "|");
-            Console.WriteLine("|" + gameData[3] + "|" + gameData[4] + "|" +  gameData[5] + "|");
-            Console.WriteLine("|" + gameData[6] + "|" + gameData[7] + "|" +  gameData[8] + "|");
+            Console.WriteLine("|" + gameData[0] + "|" + gameData[1] + "|" + gameData[2] + "|");
+            Console.WriteLine("|" + gameData[3] + "|" + gameData[4] + "|" + gameData[5] + "|");
+            Console.WriteLine("|" + gameData[6] + "|" + gameData[7] + "|" + gameData[8] + "|");
         }
     }
 }
